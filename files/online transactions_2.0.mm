32aee508a5acc1b9e83c73501a54f80cceba35f7
Prateek Garg <prateekgargmat@gmail.com>
2011-11-22 16:35:54 +0530

    Files for the second Iteration

---
+++ files/online transactions_2.0.mm
@@ -0,0 +1,47 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1321952736902" ID="ID_91178514" MODIFIED="1321952999756" TEXT="online transactions">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1321952754381" ID="ID_1391241959" MODIFIED="1321952968093" POSITION="right" TEXT="Funds transfer">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1321952770522" ID="ID_1836514251" MODIFIED="1321952968096" TEXT="sms verification">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1321952786644" HGAP="35" ID="ID_1799230057" MODIFIED="1321952982156" POSITION="left" TEXT="third party transfer" VSHIFT="-80">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1321952804820" ID="ID_678738605" MODIFIED="1321952968099" TEXT="sms verification">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1321952833131" ID="ID_163278505" MODIFIED="1321952968101" POSITION="right" TEXT="Inter bank transfer ">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1321952842849" ID="ID_1416973251" MODIFIED="1321952968103" TEXT="sms verification">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1321952856816" ID="ID_1852132957" MODIFIED="1321952968107" TEXT="rather than giving ifsc code , option of seleting bank , state and branch ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1321953003613" ID="ID_1613752791" MODIFIED="1321953143503" POSITION="left" TEXT="payment of good and services">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1321953017132" ID="ID_75809577" MODIFIED="1321953025785" TEXT="sms verification">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1321953143513" ID="ID_1618587661" MODIFIED="1321953227284" TEXT="more restictions on types of payment mode for various sites">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
